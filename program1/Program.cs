305cdd44fb7574684fc2517b657bc96999201740
2017zby <1241671642@qq.com>
2018-09-09 19:20:20 +0800

    添加项目文件。

---
+++ program1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s_1, s_2;
+            double num_1, num_2;
+            Console.Write("Please input two number :");
+            s_1 = Console.ReadLine();
+            s_2 = Console.ReadLine();
+            num_1 = Double.Parse(s_1);
+            num_2 = Double.Parse(s_2);
+            double n;
+            n = num_1 * num_2;
+            Console.WriteLine("他们的乘积是：" + n);
+        }
+    }
+}
---
+++ program2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n_1, n_2;
+            n_1 = Double.Parse(textBox1.Text);
+            n_2 = Double.Parse(textBox2.Text);
+            this.textBox3.Text = Convert.ToString(n_1 * n_2);
+        }
+    }
+}
