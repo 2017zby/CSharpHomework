8889fb7e9d4e93af5e3dc457f50160d67c76cff9
2017zby <1241671642@qq.com>
2018-10-07 22:57:34 +0800

    homework4

---
+++ homework4/program1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//书上例题
+namespace ConsoleApp1
+{
+    public class DownloadEventArgs:EventArgs
+    {
+        public double Percent;
+    }
+    public delegate void DownloadEventHandler(object sender, DownloadEventArgs e);
+
+    public class Downloader
+    {
+        public event DownloadEventHandler Downing;
+
+        public void DoDownload()
+        {
+            double total = 10000;
+            double already = 0;
+            Random rnd = new Random();
+            while (already<total)
+            {
+                System.Threading.Thread.Sleep(500);
+                already += (rnd.NextDouble() / 4) * total;
+                if (already > total) already = total;
+
+                if(Downing!=null)
+                {
+                    DownloadEventArgs args = new DownloadEventArgs();
+                    args.Percent = already / total;
+                    Downing(this, args);
+                }
+            }
+
+        }
+    }
+   public class UseDownloader
+    {
+        static void Main()
+        {
+            var downloader = new Downloader();
+            downloader.Downing += ShowProgress;
+            downloader.DoDownload();
+        }
+        static void ShowProgress(object sender ,DownloadEventArgs e)
+        {
+            Console.WriteLine($"Downloading . . . {e.Percent:##.#%}");
+        }
+    }
+}
---
+++ homework4/program1/program1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    public class ClockTimeArgs:EventArgs
+    {
+        public int Hour, Minute, Second;
+    }
+
+    public delegate void ClockTimeHandler(object sender, ClockTimeArgs e);
+
+    public class Clock
+    {
+        public event ClockTimeHandler Run;
+
+        int H, M, S;
+
+        public void SetClock()
+        {
+            H = Convert.ToInt32( Console.ReadLine());
+            M = Convert.ToInt32(Console.ReadLine());
+            S = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void DoRun()//获取现在的时间，比较小时、分、秒
+        {
+            System.DateTime currentTime = new System.DateTime();
+           
+            ClockTimeArgs args = new ClockTimeArgs();
+
+            int time = 0;
+            while (time<24*60*60)
+            {
+                time++;
+
+                int hour = DateTime.Now.Hour;
+                int minute = DateTime.Now.Minute;
+                int second = DateTime.Now.Second;
+
+                System.Threading.Thread.Sleep(1000);
+
+           
+
+                if ((hour < H) || (minute<M) || (second<S))
+                {
+                    Run(this, args);
+                }
+                if (hour>=H&&minute>=M&&second>=S)
+                {
+                    Console.WriteLine("闹钟到了！！");
+                    Console.WriteLine("/a");
+                }
+
+            }
+           
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clock = new Clock();
+            clock.SetClock();
+            clock.Run += ShowTime;
+           
+            clock.DoRun();
+        }
+
+        static void ShowTime(object sender, ClockTimeArgs e)
+        {
+            Console.WriteLine("Clock is run . . ." );
+        }
+    }
+   
+}
