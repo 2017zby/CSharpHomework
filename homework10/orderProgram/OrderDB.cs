bed5e23b1a9283513d8a72f0c5754896d927af3b
2017zby <1241671642@qq.com>
2018-11-29 21:50:32 +0800

    homework10

--- homework10/orderProgram/OrderDB.cs
+++ homework10/orderProgram/OrderDB.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace apdo_homework7
+namespace Orders
 {
-    class OrderDB
+    public class OrderDB : DbContext
     {
+        public OrderDB() : base("OrderDataBase")
+        {
+        }
+        public DbSet<Order> Order { get; set; }
+
+        public DbSet<OrderDetails> OrderDetail { get; set; }
+
     }
 }
--- homework10/orderProgram/OrderDetails.cs
+++ homework10/orderProgram/OrderDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +9,50 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
-//订单的输入操作
 namespace Orders
 {
     [Serializable]
     public class OrderDetails
     {
-        public string GoodName { set; get; }
+        [Key]
+        public string ID { set; get; }
+        public string kindOfGoods { set; get; }
         public int numberOfGoods { set; get; }
         public double price { set; get; }
         public double totalPrice { set; get; }
         public OrderDetails() { }
+        public OrderDetails(string ID, string kind, int num, double price)
+        {
+            this.ID = ID;
+            kindOfGoods = kind;
+            numberOfGoods = num;
+            this.price = price;
+            totalPrice = num * price;
+        }
         public OrderDetails(string kind, int num, double price)
         {
-            GoodName = kind;
+            OrderService os = new OrderService();
+            List<OrderDetails> odl = os.GetAllOrderDetails();
+            int a = 0;
+            for (a   = 0;  a< int.MaxValue; a++)
+            {
+                bool exist = false;
+                foreach (OrderDetails detail in odl)
+                {
+                    if (detail.ID == "LJF" + DateTime.Now.Year + DateTime.Now.Month + DateTime.Now.Day + a.ToString())
+                    {
+                        exist = true;
+                    }
+                }
+
+                if (!exist)
+                {
+                    break;
+                }
+            }
+
+            this.ID = "LJF" + DateTime.Now.Year+DateTime.Now.Month+DateTime.Now.Day + a.ToString();
+            kindOfGoods = kind;
             numberOfGoods = num;
             this.price = price;
             totalPrice = num * price;
